20c0e8b3d60eb1bb17d5a3fe30b9d5eb549900b8
Bart Menu <bart.menu@vasco.com>
2012-11-06 15:51:11 +0100

    * Comments * Better logging for System * Allow multiple public options

--- lib/Config.rb
+++ lib/Config.rb
@@ -1,8 +1,10 @@
+
+# Deals with opening and parsing a configfile
+# Also provides some utility methods to determine which is the currently active config or if a certain option was provided
 module Config
 
+  # Minimum options required. If any of them is not given, croak
   REQUIRED_OPTS = [:testlink_server,:dev_key,:prefix]
-
-
 
   def open_and_parse(conf)
     begin
@@ -60,57 +62,36 @@ module Config
   end
   module_function :open_and_parse
 
+  # A key 'public' can be given
+  # which indicates all the options which will be made public to the environment with the value given earlier in the config
+  # Public in the config is a string consisting of one or more options to publicize
+  # eg, in the configfile:
+  # public = option1
+  # => [option1]
+  # => ENV[OPTION1]
+  # public = option1,option2,option3
+  # => Array
+  # => ["option1", "option2","options3"]
   def self.expose_public(hash)
-    puts hash.inspect
     if hash.has_key?(:public)
-      # TODO
-      # pUBLIC CAN be an array OR a string
-      # eg
-      # public = option1
-      # => string
-      # public = option1,option2,option3
-      # => Array
-      # => [" basepath", "outputdir"]
-
+      public = []
       case hash[:public].class.to_s
         when /String/
           public = hash[:public].to_a
         when /Array/
-         public = hash[:public]
+          hash[:public].each  {|option| public << option.gsub(' ','')  }
         else
-          raise RuntimeError,"I just don't know what to do with myself, this type of COnfig IS ALL WRONG (expected string or hamray)'"
-
+          raise RuntimeError,"I just don't know what to do with myself, this type of COnfig IS ALL WRONG (expected string or Array)'"
       end
-
-
-
-      hash.each_key do |key|
-        puts "PUBLC:" + hash[:public].strip.inspect
-
-        if hash[:public].include?(key.to_s.strip)
-          puts 'public option ' + key.to_s + 'found, exposing!'
-          ENV[key.to_s] = hash[key]
-
-        end
-
-
-
-      end
-
-
-    else
-      puts 'No public options defined'
-
+      $log.debug("Exposing keys:#{ public.inspect}")
+      # Each listed key in public, set as an ENV variable with the value from the config
+      public.each { |key| ENV[key.to_s.upcase]  = hash[key]  }
+    else  # No public options defined
+      $log.debug("No public options defined, not exposing anything.")
     end # has key public?
-
-
-
-
   end
 
-
-
-
+  # The currently loaded/active config
   def active_config
     @config
   end
@@ -125,6 +106,7 @@ module Config
   end
   module_function :has_option?
 
+  # Compare given opts vs required opts.
   def self.validate(config_hash)
     missing = (REQUIRED_OPTS - config_hash.keys)
     raise ArgumentError,"Missing required options in config: #{missing} " if missing.length > 0
